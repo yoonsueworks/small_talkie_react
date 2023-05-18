d7d894ccac14d82d692e604e252ea88d9b434e47
yoonsue <mrokr0328@gmail.com>
2023-05-18 16:15:29 +0900

    🎉 Init: setting.json

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Page from "./pages/Page";
+import Login from "./pages/Login";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/Page" element={<Page />}></Route>
+        <Route path="/page" element={<Page />}></Route>
+        <Route path="/login" element={<Login />}></Route>
       </Routes>
     </BrowserRouter>
   );
